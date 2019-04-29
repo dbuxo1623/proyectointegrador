915ad62f13c1479b9d0fb44625b59b1b467fb42b
dbuxo1623 <49323055+dbuxo1623@users.noreply.github.com>
2019-04-29 01:33:12 -0300

    Update about.php

--- about.php
+++ about.php
@@ -69,7 +69,7 @@
              <div class="card-body">
                <h5 class="card-title">Contacto: <br> <img src="images/envelope.png" alt="e-mail"> dandelion@gmail.com <br> <img src="images/phone-call.png" alt="telefono"> (011) 4456-8756 <br> <img src="images/placeholder.png" alt="ubicacion"> Armenia 2387, Palermo </h5>
                <p class="card-text">Somos una empresa que busca ayudar a regalar. Confia en nosotros!</p>
-               <p class="card-text">Somos una empresa que busca ayudar a regalar. Confia en nosotros!</p>
+               
                <a href="#" class="btn btn-light"> Mirá los intereses!</a>
              </div>
              <div class="card-footer text-muted">
